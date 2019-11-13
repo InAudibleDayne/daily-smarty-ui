6f2a9df5a06a0f4b4d67b44c665fd13f7f5c3bb2
InAudibleDayne <daynebechtold@hotmail.com>
2019-11-13 14:18:51 -0700

    added paramaters for searchbar which will handle searching differently on different pages

--- src/components/results.js
+++ src/components/results.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Logo from "./logo";
+import SearchBar from './searchBar';
 
 class Results extends Component {
+
+    handleSearchBarSubmit(query) {
+        console.log(query);
+    }
+
   render() {
     return (
         <div>
             <Logo size={55} />
+            <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
         </div>
     );
   }
